8d6472577333eab3528c1e81ab3459bc21f50478
irvingVillarreal <92701639+irvingVillarreal@users.noreply.github.com>
2021-11-11 20:59:19 -0500

    Update calculadoraTest.php

--- calculadoraTest.php
+++ calculadoraTest.php
@@ -7,6 +7,15 @@ require_once(__DIR__.'/Calculadora.php');
 use \PHPUnit\Framework\TestCase;
 class CalculadoraTest extends TestCase{
 
+    public function sumarProveedor(){   
+        return[
+            'Caso 1' => [-1, -1, -2],
+            'Caso 2' => [0, 0, 0],
+            'Caso 3' => [0, -1, -1],
+            'Caso 4' => [-1, 0, -1]
+            ];
+    }
+
     /**
     * @dataProvider sumarProveedor
     */
@@ -17,27 +26,51 @@ class CalculadoraTest extends TestCase{
         $this->assertEquals($resultado_esperado,$calculadora->sumar($numero1,$numero2));
     }
 
+    public function restarProveedor(){   
+        return[
+            'Caso 1' => [-1, -1, 0],
+            'Caso 2' => [0, 0, 0],
+            'Caso 3' => [0, -1, 1],
+            'Caso 4' => [-1, 0, -1]
+            ];
+    }
     /**
-    * @dataProvider sumarProveedor
+    * @dataProvider restarProveedor
     */
     public function testRestar($numero1, $numero2, $resultado_esperado){
-        //$calculadora = new Calculadora();
+        $calculadora = new Calculadora();
         //$this->assertEquals(6, $calculadora->restar(10,4));
         $this->assertEquals($resultado_esperado,$calculadora->restar($numero1,$numero2));
     }
 
+    public function multiplicarProveedor(){   
+        return[
+            'Caso 1' => [-1, -1, 1],
+            'Caso 2' => [0, 0, 0],
+            'Caso 3' => [0, -1, 0],
+            'Caso 4' => [-1, 0, 0]
+            ];
+    }
     /**
-    * @dataProvider sumarProveedor
+    * @dataProvider multiplicarProveedor
     */
     public function testMultiplicar($numero1, $numero2, $resultado_esperado){
-        //$calculadora = new Calculadora();
+        $calculadora = new Calculadora();
         //$this->assertEquals(8, $calculadora->multiplicar(4,2));
         $this->assertEquals($resultado_esperado,$calculadora->multiplicar($numero1,$numero2));
     }
     
     
+    public function dividirProveedor(){   
+        return[
+            'Caso 1' => [-1, -1, 1],
+            'Caso 2' => [0, 0, 0],
+            'Caso 3' => [0, -1, 0],
+            'Caso 4' => [-1, 0, 0]
+            ];
+    }
     /**
-    * @dataProvider sumarProveedor
+    * @dataProvider dividirProveedor
     */
     public function testDividir($numero1, $numero2, $resultado_esperado){
         $calculadora = new Calculadora();
@@ -57,6 +90,7 @@ class CalculadoraTest extends TestCase{
         //$this->assertCount(5, $calculadora->generarArreglo());
         $this->assertNotEmpty($calculadora->generarArreglo());
     }
+
 
 
 }
